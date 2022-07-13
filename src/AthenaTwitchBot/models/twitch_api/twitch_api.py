3e454872a9b25a2fadba924bd698acf0a05daea2
DirectiveAthena <directive.athena@gmail.com>
2022-07-13 15:15:30 +0200

    Feat: added command start_commercial

--- src/AthenaTwitchBot/models/twitch_api/twitch_api.py
+++ src/AthenaTwitchBot/models/twitch_api/twitch_api.py
@@ -12,9 +12,11 @@ from functools import wraps
 
 # Custom Library
 import AthenaLib.HTTP.functions.requests as requests
+
 # Custom Packages
-from AthenaTwitchBot.models.twitch_api_user import TwitchApiUser
+from AthenaTwitchBot.models.twitch_api.twitch_api_user import TwitchApiUser
 from AthenaTwitchBot.data.twitch_api_urls import TwitchApiURL
+from AthenaTwitchBot.data.twitch_api_scopes import TwitchApiScopes
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Support Code -
@@ -27,6 +29,17 @@ def connected_to_twitch(fnc):
             raise ValueError(f"TwitchAPI.connect() has to be run before the {fnc} can be used")
         return await fnc(*args, **kwargs)
     return wrapper
+
+def has_scope(scope:TwitchApiScopes):
+    def decorator(fnc):
+        async def wrapper(*args,**kwargs):
+            self, *_ = args  # type: TwitchAPI
+            if scope not in self.user.scopes:
+                raise ValueError(f"The scope of '{scope.value}' was not defined on the user")
+            return await fnc(*args, **kwargs)
+        return wrapper
+    return decorator
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
@@ -43,6 +56,7 @@ class TwitchAPI:
     is_connected:bool=field(init=False, default=False)
     _loop:asyncio.AbstractEventLoop = field(init=False)
     _get_header:list[tuple[str,str]]=field(init=False, default=None)
+    _post_header_json:list[tuple[str,str]]=field(init=False, default=None)
 
     def __post_init__(self):
         self._loop = asyncio.get_running_loop()
@@ -50,13 +64,30 @@ class TwitchAPI:
             ("Authorization", f"Bearer {self.broadcaster_token}"),
             ("Client-Id", self.broadcaster_client_id)
         ]
+        self._post_header_json = [
+            *self._get_header,
+            ("Content-Type", "application/json")
+        ]
 
     # ------------------------------------------------------------------------------------------------------------------
     # - Methods that do all the magic -
     # ------------------------------------------------------------------------------------------------------------------
-    async def _get_response(self, url:str, headers:list[tuple[str,str]]):
+    async def _get(self, url:str, headers:list[tuple[str,str]]):
         try:
             response = await requests.get(url=url, headers=headers, loop=self._loop)
+            return json.loads(response.read())
+        except urllib.error.URLError as e:
+            print(e)
+            raise
+
+    async def _post(self,url:str, headers:list[tuple[str,str]],data:dict):
+
+        data_encoded = json.dumps(data).encode("utf_8")
+
+        try:
+            response = await requests.post(url=url, headers=headers, data=data_encoded, loop=self._loop)
+            if response is None:
+                return {}
             return json.loads(response.read())
         except urllib.error.URLError as e:
             print(e)
@@ -67,14 +98,24 @@ class TwitchAPI:
     # ------------------------------------------------------------------------------------------------------------------
     async def connect(self) -> dict:
         # Execute the request
-        data: dict = await self._get_response(url=TwitchApiURL.login.value,headers=self._get_header)
+        login_data, scope_data = await asyncio.gather(
+            self.login(),
+            self.get_scopes()
+        )
 
         # store the user, as some user data is required in further api commands
-        self.user = TwitchApiUser.new_from_dict(data["data"][0])
+        self.user = TwitchApiUser.new_from_dict(login_data["data"][0]).set_scopes(scope_data["scopes"])
         self.is_connected = True
 
         # return the data dictionary back
-        return data
+        return login_data
+
+    async def login(self) -> dict:
+        return await self._get(url=TwitchApiURL.login.value, headers=self._get_header)
+
+    async def get_scopes(self) -> dict:
+        return await self._get(url=TwitchApiURL.get_scopes.value,
+                               headers=[("Authorization", f"OAuth {self.broadcaster_token}")])
 
     # ------------------------------------------------------------------------------------------------------------------
     @connected_to_twitch
@@ -89,4 +130,17 @@ class TwitchAPI:
         if only_manageable_rewards:
             url += f"&only_manageable_rewards=true"
 
-        return await self._get_response(url=url, headers=self._get_header)
+        return await self._get(url=url, headers=self._get_header)
+
+    @has_scope(scope=TwitchApiScopes.ChannelEditCommercial)
+    @connected_to_twitch
+    async def start_commercial(self, *, length:int):
+        return await self._post(
+            url=TwitchApiURL.start_commercial.value,
+            headers=self._post_header_json,
+            data={
+                "broadcaster_id": self.user.id,
+                "length": length
+            }
+        )
+
