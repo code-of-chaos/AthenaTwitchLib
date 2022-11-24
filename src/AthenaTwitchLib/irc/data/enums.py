a250745535f1466705cd039e8d78205049ba501a
DirectiveAthena <directive.athena@gmail.com>
2022-11-24 21:26:21 +0100

    Cleanup: removed stale enum

--- src/AthenaTwitchLib/irc/data/enums.py
+++ src/AthenaTwitchLib/irc/data/enums.py
@@ -12,25 +12,6 @@ import enum
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
-class LineHandlerType(enum.StrEnum):
-    """
-    Types of calls that are caught by the Protocol system.
-    Used for logging.
-    """
-    PING : enum.auto()
-    SERVER_MESSAGE : enum.auto()
-    SERVER_353 : enum.auto()
-    SERVER_366 : enum.auto()
-    SERVER_CAP : enum.auto()
-    JOIN : enum.auto()
-    PART : enum.auto()
-    MESSAGE : enum.auto()
-    MESSAGE_COMMAND : enum.auto()
-    MESSAGE_COMMAND_WITHOUT_ARGS : enum.auto()
-    USER_NOTICE : enum.auto()
-    USER_STATE : enum.auto()
-    UNKNOWN : enum.auto()
-
 class BotEvent(enum.Enum):
     """
     Types of Events that can be raised inside the Bot
