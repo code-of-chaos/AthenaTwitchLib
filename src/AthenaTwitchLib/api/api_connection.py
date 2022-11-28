313972db7a871f884e1558b530e9cbc20688cf35
DirectiveAthena <directive.athena@gmail.com>
2022-11-28 14:11:34 +0100

    Feat: http execute command simplified

--- src/AthenaTwitchLib/api/api_connection.py
+++ src/AthenaTwitchLib/api/api_connection.py
@@ -3,22 +3,22 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # General Packages
 from __future__ import annotations
+
 import dataclasses
+import json
 from dataclasses import dataclass, field
 import aiohttp
 from typing import Callable, AsyncGenerator
-import uuid
 
 from AthenaTwitchLib.api._token_data import TokenData
 # Athena Packages
 
 # Local Imports
-from AthenaTwitchLib.api.data.urls import TwitchApiUrl
-from AthenaTwitchLib.api.data.enums import DataFromConnection, HttpCommand
+from AthenaTwitchLib.api.data.enums import DataFromConnection, HttpMethod
 from AthenaTwitchLib.logger import ApiLogger, SectionAPI
 from AthenaTwitchLib.api._request_data import RequestData
 from AthenaTwitchLib.api._user_data import UserData
-from AthenaTwitchLib.api.requests import ConnectionRequests
+from AthenaTwitchLib.api.requests import ConnectionRequests, ApiRequests
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Support Code -
@@ -26,26 +26,51 @@ from AthenaTwitchLib.api.requests import ConnectionRequests
 _mapping_data_from_connection: dict[DataFromConnection:Callable] = {
     DataFromConnection.BROADCASTER_ID : lambda api_conn: api_conn.user.id,
     DataFromConnection.MODERATOR_ID : lambda api_conn: api_conn.user.id,
+    DataFromConnection.OATH_TOKEN : lambda api_conn: api_conn.oath_token
 }
+
+# Create the simple Hash switch for the `ApiConnection._http_execute`
+_http_execute_switch = {
+    HttpMethod.GET:     lambda session,url,*,params,     **_:   session.get(url, params=params),
+    HttpMethod.POST:    lambda session,url,*,params,data,**_:   session.post(url, data=data, params=params),
+    HttpMethod.PUT:     lambda session,url,*,params,data,**_:   session.put(url, data=data, params=params),
+    HttpMethod.PATCH:   lambda session,url,*,params,data,**_:   session.patch(url, data=data, params=params),
+    HttpMethod.DELETE:  lambda session,url,*,params,data,**_:   session.delete(url, data=data, params=params),
+}
+
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
 @dataclass(slots=True, kw_only=True)
 class ApiConnection:
-    username:str
     oath_token:str
-    client_id:str
 
-    # init False
-    _headers_auth:dict = field(init=False)
+    # Non init
     user:UserData = field(init=False, default=None)
     token:TokenData = field(init=False, default=None)
 
-    def __post_init__(self):
-        self._headers_auth = {
-            "Authorization": f"Bearer {self.oath_token}",
-            "Client-Id": self.client_id,
-        }
+    # Extra data needed for special cases
+    _headers_auth:dict = field(init=False, default=None)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # - Properties -
+    # ------------------------------------------------------------------------------------------------------------------
+    @property
+    def headers_auth(self):
+        if self._headers_auth is None:
+            if self.token is None:
+                raise PermissionError(
+                    "Token has never been validated and stored to connection. "
+                    "This can easily be resolved by using async with"
+                )
+
+            # Define if it hasn't been defined yet
+            self._headers_auth = {
+                "Authorization": f"Bearer {self.oath_token}",
+                "Client-Id": self.token.client_id,
+            }
+
+        return self._headers_auth
 
     # ------------------------------------------------------------------------------------------------------------------
     # - Context managed -
@@ -53,11 +78,13 @@ class ApiConnection:
     async def __aenter__(self):
         # make sure that the user has at least logged in once to the api
         #   to get correct information
-        if self.user is None:
-            self.user = await self.get_user_data()
-
         if self.token is None:
             self.token = await self.validate_token()
+
+        if self.user is None:
+            user_data = await self._http_execute(ApiRequests.get_users(client_id=self.token.user_id))
+            ApiLogger.log_debug(section=SectionAPI.USER_DATA, data=user_data)
+            self.user = UserData(**user_data["data"][0])
 
         return self
 
@@ -74,40 +101,31 @@ class ApiConnection:
     # - Special Http commands -
     # ------------------------------------------------------------------------------------------------------------------
     async def validate_token(self) -> TokenData:
-        token_data = await self.get(
+        # Check if the token is valid
+        #   When no longer valid, the API will throw an error
+        token_data = await self._http_execute(
             ConnectionRequests.validate_token(self.oath_token),
-            check_scope=False
+            check_scopes=False
         )
-        ApiLogger.log_debug(section=SectionAPI.USER_DATA, data=token_data)
-        return TokenData(**token_data)
 
-    async def get_user_data(self) -> UserData:
-        user_data = await self.get(
-            ConnectionRequests.get_user(username=self.username),
-            check_scope=False
-        )
-        ApiLogger.log_debug(section=SectionAPI.USER_DATA, data=user_data)
-        return UserData(**user_data["data"][0])
+        match token_data:
+            # Normal flow of data
+            case {"client_id":client_id,"login": login,"scopes": scopes, "user_id": user_id, "expires_in": expires_in}:
+                ApiLogger.log_debug(section=SectionAPI.TOKEN_DATA, data=token_data)
+                return TokenData(
+                    client_id=client_id,
+                    login=login, scopes=scopes,
+                    user_id=user_id,
+                    expires_in=expires_in
+                )
+
+            # Token has an error
+            case {"status":_, "message":_}:
+                raise PermissionError(token_data)
 
     # ------------------------------------------------------------------------------------------------------------------
     # - Http commands -
     # ------------------------------------------------------------------------------------------------------------------
-    async def _http_switch(self,request_data:RequestData) -> dict:
-        ApiLogger.log_debug(section=SectionAPI.REQUEST_SEND, data=request_data)
-        match request_data:
-
-            case RequestData(http_command=HttpCommand.GET):
-                result = await self.get(request_data)
-
-            case RequestData(http_command=HttpCommand.POST):
-                result = await self.post(request_data)
-
-            case _:
-                raise ValueError
-
-        ApiLogger.log_debug(section=SectionAPI.REQUEST_RESULT, data=result)
-        return result
-
     async def request(self,request_data:RequestData,*,limit:int=None) -> AsyncGenerator[dict, None]:
         """
         Creates a request to the Twitch API.
@@ -124,7 +142,7 @@ class ApiConnection:
         while pagination:
 
             # Get the result from the new request
-            match (result := await self._http_switch(request_data)):
+            match (result := await self._http_execute(request_data)):
                 # Normal flow of data
                 case {"data": data,}:
                     #   Go over items
@@ -159,39 +177,35 @@ class ApiConnection:
                 case _:
                     raise ValueError(result)
 
-    async def get(self, request_data:RequestData, check_scope:bool=True) -> dict:
-        # Check if all scopes are present
-        if check_scope and not (token_scopes := self.token.scopes) >= (request_scopes := request_data.scopes):
+    async def _http_execute(self,request_data:RequestData, check_scopes:bool=True) -> dict:
+        ApiLogger.log_debug(section=SectionAPI.REQUEST_SEND, data=request_data)
+
+        # Check if all scopes are present on the Oath Token
+        #   This shouldn't be skipped, except for the original request of token validation
+        #   Checks if all `request_scopes` is in `token_scopes`
+        if check_scopes and not (token_scopes := self.token.scopes) >= (request_scopes := request_data.scopes):
+            ApiLogger.log_warning(section=SectionAPI.TOKEN_INVALID, data=token_scopes)
             raise PermissionError(
                 f"Token did not have required scopes:\nToken Scopes: {token_scopes}\nScopes required: {request_scopes}"
             )
 
-        # make a union of both dictionaries
-        #   Don't store them to the request_data, as this is a frozen dataclass
-        headers = request_data.headers
-        if request_data.header_include_oath:
-             headers |= self._headers_auth
-
+        # Assemble all components with extra data
+        #   Don't update the already set request_data as this with pagination the original data might be needed
+        headers = request_data.headers | (self.headers_auth if request_data.header_include_oath else {})
         params = request_data.params | self.get_from_connection(request_data.params_from_connection)
-
-        async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.get(request_data.url, params=params) as response:
-                return await response.json()
-
-    async def post(self, request_data:RequestData, check_scope:bool=True) -> dict:
-        # Check if all scopes are present
-        if check_scope and not (token_scopes := self.token.scopes) >= (request_scopes := request_data.scopes):
-            raise PermissionError(
-                f"Token did not have required scopes:\nToken Scopes: {token_scopes}\nScopes required: {request_scopes}"
-            )
-
-        # make a union of both dictionaries
-        #   Don't store them to the request_data, as this is a frozen dataclass
-        headers = request_data.headers | self._headers_auth
         data = request_data.data | self.get_from_connection(request_data.data_from_connection)
-        params = request_data.params | self.get_from_connection(request_data.params_from_connection)
 
+        # Enter the session,
+        #   so it will be closed correctly
         async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.post(request_data.url, data=data, params=params) as response:
-                return await response.json()
+            async with _http_execute_switch[request_data.http_method](
+                    session,
+                    request_data.url,
+                    params=params,
+                    data=data
+            ) as response:
+                result = await response.json()
 
+        # Log output
+        ApiLogger.log_debug(section=SectionAPI.REQUEST_RESULT, data=result)
+        return result
