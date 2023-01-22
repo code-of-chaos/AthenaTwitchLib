e836be5a1d4de0b9c714aa089db139118f2848c9
DirectiveAthena <directive.athena@gmail.com>
2023-01-22 18:40:39 +0100

    Feat: Optimized data_received function

--- src/AthenaTwitchLib/irc/data/regex.py
+++ src/AthenaTwitchLib/irc/data/regex.py
@@ -12,6 +12,8 @@ import re
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
+server_ping = re.compile(r"PING :tmi\.twitch\.tv")
+
 server_message = re.compile(r"^:tmi.twitch.tv \d\d\d ([^ ]*) :")
 server_353 = re.compile(r"^:([^ ]*)\.tmi\.twitch\.tv 353 \1 = #([^ ]*) :")
 server_366 = re.compile(r"^:([^ ]*)\.tmi\.twitch\.tv 366 \1 #([^ ]*) :End of /NAMES list$")
@@ -21,7 +23,6 @@ join = re.compile(r"^:([^ ]*)!\1@\1\.tmi\.twitch\.tv JOIN (#.*)")
 part = re.compile(r"^:([^ ]*)!\1@\1\.tmi\.twitch\.tv PART (#.*)")
 
 message = re.compile(r"^@([^ ]*) ([^ ]*) PRIVMSG #([^:]*) :(.*)")
-message_command = re.compile(r"^([!.?#])([^ ]*)(.*)")
 
 user_notice = re.compile(r"^([^ ]*) ([^ ]*) USERNOTICE #([^:]*) :(.*)")
 user_notice_raid = re.compile(r"^([^ ]*msg-id=raid[^ ]*) :tmi.twitch.tv USERNOTICE #([^ ]*)$")
