b1848f1d3f44be6072a7e8542327475a9c3653fb
DirectiveAthena <directive.athena@gmail.com>
2023-01-22 22:20:04 +0100

    Refactor: Move _create_connection to it's own method
    This commit was made possible by: barankalayc7
    With the following message : "that's all good"

--- src/AthenaTwitchLib/irc/irc_connection.py
+++ src/AthenaTwitchLib/irc/irc_connection.py
@@ -28,16 +28,13 @@ class IrcConnection:
     Relies on BotData to control the access to the Twitch IRC chat
     Relies on logic_commands and logic_tasks for the connection to be populated with custom functionality
     """
-    bot_data:BotData
-
     ssl_enabled: bool = True
     host: str = 'irc.chat.twitch.tv'
     port: int = 6667
     port_ssl: int = 6697
 
-    restart_attempts:int=5 # if set to -1, will run forever
+    connect_attempts:int=5
 
-    conn_event: asyncio.Future = None
     conn_protocol: type[IrcConnectionProtocol] = IrcConnectionProtocol
 
     logic_commands:BaseCommandLogic = field(default_factory=CommandLogic)
@@ -47,68 +44,72 @@ class IrcConnection:
     _current_restart_attempt:int=field(init=False, default=0)
     _loop: asyncio.AbstractEventLoop = field(init=False, default_factory=asyncio.get_running_loop)
 
-    def __post_init__(self):
-        if self.conn_event is None:
-            self.conn_event = self._loop.create_future()
-
     # ------------------------------------------------------------------------------------------------------------------
     # - Support methods -
     # ------------------------------------------------------------------------------------------------------------------
-    def _socket_factory(self) -> socket.socket:
-        """
-        Simple method that creates the required socket for the asyncio.Protocol factory
-        """
+    async def _create_connection(self, bot_data:BotData) -> tuple[asyncio.Future, asyncio.BaseTransport,IrcConnectionProtocol]:
+        # Creates the socket
         sock:socket.socket = socket.socket()
         sock.settimeout(5.)
         sock.connect((
             self.host,
-            self.port_ssl if self.ssl_enabled else self.port
-        ))
-        return sock
+            self.port_ssl if self.ssl_enabled else self.port)
+        )
+
+        # Creates the connection's event tied to the connection
+        conn_event = self._loop.create_future()
+
+        # Assemble the asyncio.Protocol
+        #   The custom protocol_type needs a constructor to known which patterns to use, settings, etc...
+        transport, protocol_obj = await self._loop.create_connection(
+            protocol_factory=functools.partial(
+                self.conn_protocol,
+
+                # Assign the logic
+                #   If this isn't defined, the protocol can't handle anything correctly
+                bot_data=bot_data,
+                logic_commands=self.logic_commands,
+
+                # For restarts, exits and other special events
+                #   The functions following out of this require the connection class for some things
+                conn_event=conn_event
+            ),
+            server_hostname=self.host,
+            ssl=self.ssl_enabled,
+            sock=sock,
+        )
+
+        # Once everything is established, we can return the result
+        return conn_event, transport, protocol_obj
 
     # ------------------------------------------------------------------------------------------------------------------
     # - Main Methods -
     # ------------------------------------------------------------------------------------------------------------------
-    async def connect(self) -> None:
+    async def connect(self, bot_data:BotData) -> None:
         """
         Constructor function for the BotData and all its logical systems like the asyncio.Protocol handler.
         It also logs the irc in onto the Twitch IRC server
         """
-        while self.restart_attempts != 0:
+        while True:
+            for _ in range(self.connect_attempts):
+                conn_event, transport, protocol_obj = await self._create_connection(bot_data=bot_data)
+                if transport:
+                    IrcLogger.log_debug(section=SectionIRC.CONNECTION_MADE)
+                    break
 
-            # Assemble the asyncio.Protocol
-            #   The custom protocol_type needs a constructor to known which patterns to use, settings, etc...
-            transport, protocol_obj = await self._loop.create_connection(
-                protocol_factory=functools.partial(
-                    self.conn_protocol,
-
-                    # Assign the logic
-                    #   If this isn't defined, the protocol can't handle anything correctly
-                    bot_data=self.bot_data,
-                    logic_commands=self.logic_commands,
-
-                    # For restarts, exits and other special events
-                    #   The functions following out of this require the connection class for some things
-                    conn_event=self.conn_event
-                ),
-                server_hostname=self.host,
-                ssl=self.ssl_enabled,
-                sock=self._socket_factory(),
-            )
-            if transport is None:
-                IrcLogger.log_error(section=SectionIRC.CONNECTION_REFUSED)
-                raise ConnectionRefusedError
+            # Break did not occur,
+            #   meaning no connection was established
             else:
-                IrcLogger.log_debug(section=SectionIRC.CONNECTION_MADE)
+                IrcLogger.log_error(section=SectionIRC.CONNECTION_REFUSED)
+                raise ConnectionError
 
             # noinspection PyTypeChecker
             self.logic_tasks.start_all_tasks(transport, self._loop)
 
             # Waiting portion of the IrcConnection,
             #   This regulates the irc starting back up and restarting
-            match await self.conn_event:
+            match await conn_event:
                 case ConnectionEvent.RESTART:
-                    self._current_restart_attempt +=1
                     print(f"{NEW_LINE*25}{'-'*25}RESTART ATTEMPT {self._current_restart_attempt}{'-'*25}")
                     IrcLogger.log_warning(section=SectionIRC.CONNECTION_RESTART,
                                           data=f"attempt={self._current_restart_attempt}")
@@ -125,11 +126,9 @@ class IrcConnection:
                     self.logic_tasks.stop_all_tasks()
 
                     # restarts it all
-                    self.restart_attempts -= 1
                     continue
 
                 case ConnectionEvent.EXIT | _:
                     IrcLogger.log_warning(section=SectionIRC.CONNECTION_EXIT, data=f"called by ConnectionEvent")
                     self.logic_tasks.stop_all_tasks()
-                    self._loop.stop()
                     break
